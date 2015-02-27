6e1d1969db5c6db6e22163fdee71901a77ff544f
Juan P. Mata <jpmata1@gmail.com>
2015-02-26 22:34:09 -0600

    Packet number read
    started to read the packet number from received packet. NOT Tested Yet.

--- responder.c
+++ responder.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+
 int main ( int argc, char *argv[] )
 {
 	int i;
@@ -13,6 +14,11 @@ int main ( int argc, char *argv[] )
 	struct sockaddr_in servaddr, cliaddr;
 	socklen_t len;
 	char mesg[1000];
+
+	int cache[5] = { 0 };
+	char pCharNum[4];
+	int packetNum;
+	//char *buffer;
 
 	for(i = 0; i < argc; ++i)
 	{
@@ -40,6 +46,16 @@ int main ( int argc, char *argv[] )
    {
       len = sizeof(cliaddr);
       n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+      //get packet number and convert to int
+      for(i = 0; i< 5; i++){pCharNum[i] = mesg[i];} 
+      packetNum = atoi(pCharNum);
+  	  printf("Packet Num: %d\n", packetNum);
+  	  //check cache for if we already have it
+      if(cache[packetNum % 5] == 0)
+      {
+      	cache[packetNum % 5] = 1;
+      	//put message in storage
+      }
       sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
 //      printf("-------------------------------------------------------\n");
       mesg[n] = 0;
